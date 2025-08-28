d3cc98b110d61d2bf797b465440408617722da31
Nino Gergonne <ngergonne2@huskers.unl.edu>
2025-08-28 11:36:14 -0500

    added my major ro the out put and fixed previous errors in code

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Authors: Owen Strazdas, Nino Gergonne
+ *
+ * Prints the names and Majors of Owen and Nino
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("My name is Nino Gergonne!\n");
+    printf("My Major is Computer Science!\n");
+
+    return 0;
+}
