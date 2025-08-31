a20183de0cd037808a1dfff0ff1d87e31425b217
NinoAG <ngergonne2@husker.unl.edu>
2025-08-31 16:38:03 -0500

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,22 @@
+/**
+ * Authors: Owen Strazdas, Nino Gergonne
+ *
+ *Simple C code that prints the names and Majors of Owen and Nino
+'*/
+
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main(int argc, char **argv) {
+
+
+    printf("My name is Nino Gergonne!\n");
+    printf("My Major is Computer Science!\n");
+
+
+    return 0;
+
+
+}
