62f0a4da777851659ef0406fc10322c2fe8058c4
StrazzleDazzle <odogstraz@gmail.com>
2025-08-31 17:10:43 -0500

    added my major to the output (Owen)

--- hello.c
+++ hello.c
@@ -15,7 +15,10 @@ int main(int argc, char **argv) {
     printf("My name is Nino Gergonne!\n");
     printf("My Major is Computer Science!\n");
 
+    printf("My name is Owen Strazdas!\n");
+    printf("My major is Electrical Engineering!\n");
 
+    
     return 0;
 
 
